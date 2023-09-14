441392dadd1796f9ea8795d7d788f8c48b078d30
Damian Peiris <drpeiris3@gmail.com>
2023-09-14 23:09:55 +0530

    Completed the 'TechLeadController' endpoint.

--- src/main/java/com/damian/employeemanager/endpoints/TechLeadController.java
+++ src/main/java/com/damian/employeemanager/endpoints/TechLeadController.java
@@ -14,11 +14,37 @@ import org.springframework.web.bind.annotation.*;
 public class TechLeadController {
     @Autowired
     private TechLeadService techLeadService;
-    @PostMapping(path = "/save",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response save(@RequestBody TechLead_DTO techLead_dto){
+
+    @PostMapping(path = "/save", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response save(@RequestBody TechLead_DTO techLead_dto) {
         return techLeadService.save(techLead_dto);
 
     }
+
+    @PutMapping(path = "/update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public Response update(@RequestBody TechLead_DTO techLead_dto) {
+        return techLeadService.update(techLead_dto);
+
+    }
+
+    @GetMapping(path = "/search", params = "techLeadId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response search(@RequestParam("techLeadId") String techLeadId) {
+        return techLeadService.search(techLeadId);
+
+
+    }
+
+    @DeleteMapping(path = "/delete", params = "techLeadId", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response delete(@RequestParam("techLeadId") String techLeadId) {
+        return techLeadService.delete(techLeadId);
+    }
+
+    @GetMapping(path = "/fetchAll", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Response getAll() {
+        return techLeadService.getAll();
+    }
+
 
 
 }
