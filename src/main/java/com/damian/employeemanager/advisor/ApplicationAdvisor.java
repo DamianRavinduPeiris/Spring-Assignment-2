e679a9369921841321ea53090763a2d1efaab2e2
Damian Peiris <drpeiris3@gmail.com>
2023-09-14 22:13:27 +0530

    Added an Exception handler + Response handler.

---
+++ src/main/java/com/damian/employeemanager/advisor/ApplicationAdvisor.java
@@ -0,0 +1,23 @@
+package com.damian.employeemanager.advisor;
+
+import com.damian.employeemanager.response.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Component
+public class ApplicationAdvisor {
+    @Autowired
+    private Response response;
+    @ExceptionHandler({Exception.class})
+    public Response handleExceptions(Exception exception){
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        response.setMessage("Server threw an exception : "+exception.getLocalizedMessage());
+        response.setData(null);
+        return response;
+
+    }
+}
--- src/main/java/com/damian/employeemanager/config/WebAppConfig.java
+++ src/main/java/com/damian/employeemanager/config/WebAppConfig.java
@@ -1,5 +1,7 @@
 package com.damian.employeemanager.config;
 
+import com.damian.employeemanager.advisor.ApplicationAdvisor;
+import com.damian.employeemanager.response.Response;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackageClasses = {Response.class, ApplicationAdvisor.class})
 @EnableWebMvc
 public class WebAppConfig {
     @Bean
